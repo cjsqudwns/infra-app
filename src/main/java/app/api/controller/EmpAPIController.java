96f26cf77418efe2809b2093bffcb463be659c90
yhy0009 <ref0009@naver.com>
2025-05-14 16:04:34 +0900

    feat add delete method

--- src/main/java/app/api/controller/EmpAPIController.java
+++ src/main/java/app/api/controller/EmpAPIController.java
@@ -1,15 +1,20 @@
 package app.api.controller;
 
-import app.repository.EmpRepository;
+import app.entity.Emp;
 import app.service.EmpService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RequiredArgsConstructor
+import org.springframework.web.bind.annotation.*;
+
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api")
 public class EmpAPIController {
 
     private final EmpService empService;
+
+    @DeleteMapping("/emp/{empno}")
+    public Emp deleteEmp(@PathVariable Integer empno) {
+        return empService.deleteEmp(empno);
+    }
 }
--- src/main/java/app/service/EmpService.java
+++ src/main/java/app/service/EmpService.java
@@ -1,12 +1,26 @@
 package app.service;
 
+import app.entity.Emp;
 import app.repository.EmpRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @RequiredArgsConstructor
 @Service
+@Transactional
 public class EmpService {
 
     private final EmpRepository empRepository;
+    
+    @Transactional
+    public Emp deleteEmp(Integer empno) {
+        Emp emp = empRepository.findById(empno)
+                .orElseThrow(() -> new EntityNotFoundException("사원이 존재하지 않습니다. empno=" + empno));
+        empRepository.delete(emp);
+        return emp;
+    }
+    
 }
