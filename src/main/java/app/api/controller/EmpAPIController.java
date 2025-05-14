6fdaaa6427a722826b231ad53f9b50f5451b8c84
siiiirru <eqeee@naver.com>
2025-05-14 15:42:29 +0900

    feat:emp 조회 기능 추가

--- src/main/java/app/api/controller/EmpAPIController.java
+++ src/main/java/app/api/controller/EmpAPIController.java
@@ -1,8 +1,12 @@
 package app.api.controller;
 
+import app.entity.Emp;
 import app.repository.EmpRepository;
 import app.service.EmpService;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +16,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class EmpAPIController {
 
     private final EmpService empService;
+    
+    @GetMapping("/emp/{empno}")
+	public Emp getDeptByDeptno(@PathVariable Integer empno) {
+		Emp emp = empService.getEmpByEmpno(empno);
+		return emp;
+	}
 }
--- src/main/java/app/service/EmpService.java
+++ src/main/java/app/service/EmpService.java
@@ -1,6 +1,9 @@
 package app.service;
 
+import app.entity.Emp;
 import app.repository.EmpRepository;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,4 +12,10 @@ import org.springframework.stereotype.Service;
 public class EmpService {
 
     private final EmpRepository empRepository;
+    
+    @Transactional
+    public Emp getEmpByEmpno(Integer empno) {
+    	return empRepository.findById(empno)
+    			.orElseThrow(() -> new EntityNotFoundException("사원 정보가 존재하지 않습니다."));
+    }
 }
